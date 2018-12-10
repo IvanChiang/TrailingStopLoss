05cf0201fedcbd35b2517a9e6d66a32983a3453c
Ivan <bkgrslbhee@gmail.com>
2018-12-11 00:31:27 +0800

    change Trailing stop loss to pos Mgn

--- utility.py
+++ utility.py
@@ -1,19 +1,25 @@
-import pandas as pd
-from pathlib import Path
 import logging
+from pathlib import Path
+
+import pandas as pd
+from pushbullet import Pushbullet
 
 log = logging.getLogger(__name__)
 
 
-class TrailingStopLoss:
-    COLUMN = [str(i) for i in range(20)] + ['current_price', 'stop_percent', 'trailing_price', 'enabled']
+class PositionMgn:
+    COLUMN = [str(i) for i in range(22)] + ['current_price', 'stop_percent', 'trailing_price', 'enabled']
     FILE_NAME = r"TSLManager.csv"
 
-    def __init__(self, order, curr, percent, evt):
+    def __init__(self):
+
+        self.pb = Pushbullet(r'o.rLhRu7uqYBERqfd473GTZcRKOQY1Ok0V')
+
         posFile = Path(self.FILE_NAME)
         if posFile.exists():
             log.debug("position file exist! load from it!")
-            self.__data = pd.read_csv(posFile)
+            self.__data = pd.read_csv(posFile, index_col=0)
+            log.debug("\n%s", self.__data)
         else:
             log.debug("position file not exist! new an empty data!")
             self.__data = pd.DataFrame(columns=self.COLUMN)
@@ -25,15 +31,26 @@ class TrailingStopLoss:
 
     def add(self, monitor_target):
         monitor_target_l = monitor_target.split(',')
-        stock_id = monitor_target_l[3]
-        if stock_id not in self.__data.index and self.__data.at[stock_id, 'enabled'] == 1:
-            self.__data.loc[stock_id] = monitor_target_l
-            self.__data.loc[stock_id, 'enabled'] = 1
-            curr_price = self.__data.at[stock_id, 'current_price'] = monitor_target_l[4]
-            stop_percent = self.__data.at[stock_id, 'stop_percent'] = 0.1
-            self.__data.at[stock_id, 'trailing_price'] = curr_price - curr_price * stop_percent
+        stock_id = monitor_target_l[2]
+        if stock_id not in self.__data.index:
+            curr_price = float(monitor_target_l[5])
+            stop_percent = 0.1
+            self.__data.loc[stock_id] = monitor_target_l + [curr_price, stop_percent,
+                                                            (curr_price - curr_price * stop_percent), 1]
+
+        log.debug("\n%s", self.__data)
+
+        self.sync()
+
+    def getSubscribeList(self):
+        return self.__data.index
+
+    def sync(self):
+        # sync to file
+        self.__data.to_csv(self.FILE_NAME)
 
     def update(self, stock_id, curr_price):
+        log.debug('update: %s, %f', stock_id, curr_price)
         self.__data.loc[stock_id, 'current_price'] = curr_price
 
         stop_percent = self.__data.loc[stock_id, 'stop_percent']
@@ -42,13 +59,11 @@ class TrailingStopLoss:
             self.__data.loc[stock_id, 'trailing_price'] = curr_price - curr_price * stop_percent
 
         if curr_price < trailing_price:
-            self.__stopLostEvt()
-            # self.remove()
+            self.pb.push_note(stock_id, "stop loss now!")
 
             self.__data.loc[stock_id, 'enabled'] = 0
 
-        # sync to file
-        self.__data.to_csv(self.FILE_NAME)
+        self.sync()
 
     def remove(self):
         del self
